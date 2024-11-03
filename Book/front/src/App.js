1180cb7bea9d8d053c1febb6b3b2a5939cf227d1
0000yuyu <yujin09219@naver.com>
2024-11-04 08:33:47 +0900

    [frontend - 송유진] 2024.11.04 App 구조 추가

--- Book/front/src/App.js
+++ Book/front/src/App.js
@@ -7,9 +7,15 @@ import MainBanner from './components/MainBanner/mainbanner';
 import Membership from './components/Membership/membership';
 import Mypage from './components/Mypage/mypage';
 import FindPWD from './components/FindID/FindPWD';
+import BookDetail from './components/BookDetail/BookDetail';
+import Classification from './components/Classification/Classification';
+import BookInfo from './components/BookInfo/BookInfo';
+import ShoppingCart from './components/ShoppingCart/ShoppingCart'
+import Purchase from './components/Purchase/Purchase'
+import Subscribe from './components/Subscribe/Subscribe'
 
 const App = () => {
-    return (
+return(
         <Router>
             <MainBanner />
             <Routes>
@@ -19,6 +25,12 @@ const App = () => {
                 <Route path="/membership" element={<Membership />} />
                 <Route path="/mypage/*" element={<Mypage />} />
                 <Route path="/findpwd" element={<FindPWD />} />
+                <Route path="/detail" element={<BookDetail />} />
+                <Route path="/classification" element={<Classification />} />
+                <Route path="/book/:id" element={<BookInfo />} />
+                <Route path="/cart" element={<ShoppingCart />} />
+                <Route path="/purchase" element={<Purchase />} />
+                <Route path="/subscribe" element={<Subscribe />} />
             </Routes>
         </Router>
     );
