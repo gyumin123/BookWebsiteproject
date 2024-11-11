dd6bc7c7e27fd24d79ba7af3ea3ae2be205a5afc
0000yuyu <yujin09219@naver.com>
2024-11-11 11:07:49 +0900

    [frontend - 송유진] 2024.11.07 라우터 구조 수정

--- Book/front/src/App.js
+++ Book/front/src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FindID from './components/FindID/FindID';
 import HOME from './components/Home/home';
 import Login from './components/Login/login';
-import MainBanner from './components/MainBanner/mainbanner';
+import Header from './components/Banner/header';
+import Footer from './components/Banner/footer';
 import Membership from './components/Membership/membership';
 import Mypage from './components/Mypage/mypage';
 import FindPWD from './components/FindID/FindPWD';
@@ -13,11 +14,12 @@ import BookInfo from './components/BookInfo/BookInfo';
 import ShoppingCart from './components/ShoppingCart/ShoppingCart'
 import Purchase from './components/Purchase/Purchase'
 import Subscribe from './components/Subscribe/Subscribe'
+import Support from './components/Support/Support'
 
 const App = () => {
 return(
         <Router>
-            <MainBanner />
+            <Header />
             <Routes>
                 <Route path="/findID" element={<FindID />} />
                 <Route path="/login" element={<Login />} />
@@ -27,11 +29,13 @@ return(
                 <Route path="/findpwd" element={<FindPWD />} />
                 <Route path="/detail" element={<BookDetail />} />
                 <Route path="/classification" element={<Classification />} />
-                <Route path="/book/:id" element={<BookInfo />} />
+                <Route path="/book/*" element={<BookInfo />} />
                 <Route path="/cart" element={<ShoppingCart />} />
                 <Route path="/purchase" element={<Purchase />} />
                 <Route path="/subscribe" element={<Subscribe />} />
+                <Route path="/support/*" element={<Support />} />
             </Routes>
+            <Footer />
         </Router>
     );
 };
