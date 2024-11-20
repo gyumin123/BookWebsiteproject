87422b307bc8a45c89bb2a5773abb0591a8da852
DESKTOP-ST9JOTO\SAMSUNG <krumin8384@gmail.com>
2024-11-20 15:44:13 +0900

    [backend - 박규민] 2024.11.20- 장바구니 관련 api 수정

--- Book/src/main/java/com/book/cart/CartItem.java
+++ Book/src/main/java/com/book/cart/CartItem.java
@@ -6,12 +6,12 @@ import jakarta.persistence.*;
 @Table(name = "cart")
 public class CartItem {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id; // 책 아이디
 
-    private String purchaseType;
-    private Integer period;
-    private Double price;
+    private String userid; // 유저 아이디
+    private String purchaseType; // 대여/소장
+    private Integer period; // 기간
+    private Double price; // 총 가격
 
     // Getters and Setters
     public Long getId() {
@@ -20,6 +20,14 @@ public class CartItem {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userId) {
+        this.userid = userId;
     }
 
     public String getPurchaseType() {
